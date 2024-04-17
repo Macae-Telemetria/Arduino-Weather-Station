3a9f6664121ef7f06a94c86587a70e6f0a3ae6c0
Dardrilkael <gabrielfonsecabarreto@gmail.com>
2024-04-17 12:36:18 -0300

    Fix: data.h

--- arduino/index/data.h
+++ arduino/index/data.h
@@ -1,5 +1,5 @@
-#include <Arduino.h>
-
+#pragma once
+// --- Config data  ---
 struct Config {
     char station_uid[64];
     char station_name[64];
@@ -20,83 +20,86 @@ struct Config {
     int interval;
 };
 
-bool parseConfigString(const char* configString, Config& config) {
-    const char* delimiter = "\t";
-    char* configCopy = strdup(configString); // Make a copy of the input string
-    if (configCopy == nullptr) return false; // Error: memory allocation failed
+struct Config config;
+const char *configFileName = "/config.txt";
 
-    char* token;
-    char* saveptr;
-    token = strtok_r(configCopy, delimiter, &saveptr);
-    if (token == nullptr) {
-        free(configCopy); // Free allocated memory before returning
-        return false; // Error: empty string
-    }
+// --- HeachCheck data  ---
 
-    while (token != nullptr) {
-        char* colon = strchr(token, ':');
-        if (colon != nullptr) {
-            *colon = '\0'; // Null terminate to separate variable name and value
-            const char* varName = token;
-            const char* varValue = colon + 1; // Move past the colon
-            // Now you have variable name and value, you can handle them accordingly
-            // Here, we'll store the values in the Config struct based on the variable name
+struct HealthCheck {
+  const char *softwareVersion;
+  int timestamp;
+  bool isWifiConnected;
+  bool isMqttConnected;
+  int wifiDbmLevel;
+  int timeRemaining;
+};
 
-            if (strcmp(varName, "STATION_UID") == 0) {
-                strcpy(config.station_uid, varValue);
-            } else if (strcmp(varName, "STATION_NAME") == 0) {
-                strcpy(config.station_name, varValue);
-            } else if (strcmp(varName, "WIFI_SSID") == 0) {
-                strcpy(config.wifi_ssid, varValue);
-            } else if (strcmp(varName, "WIFI_PASSWORD") == 0) {
-                strcpy(config.wifi_password, varValue);
-            } else if (strcmp(varName, "MQTT_SERVER") == 0) {
-                strcpy(config.mqtt_server, varValue);
-            } else if (strcmp(varName, "MQTT_USERNAME") == 0) {
-                strcpy(config.mqtt_username, varValue);
-            } else if (strcmp(varName, "MQTT_PASSWORD") == 0) {
-                strcpy(config.mqtt_password, varValue);
-            } else if (strcmp(varName, "MQTT_TOPIC") == 0) {
-                strcpy(config.mqtt_topic, varValue);
-            } else if (strcmp(varName, "MQTT_PORT") == 0) {
-                config.mqtt_port = atoi(varValue);
-            } else if (strcmp(varName, "INTERVAL") == 0) {
-                config.interval = atoi(varValue);
-            } else if (strcmp(varName, "MQTT_HOSTV2_SERVER") == 0) {
-                strcpy(config.mqtt_hostV2_server, varValue);
-            } else if (strcmp(varName, "MQTT_HOSTV2_USERNAME") == 0) {
-                strcpy(config.mqtt_hostV2_username, varValue);
-            } else if (strcmp(varName, "MQTT_HOSTV2_PASSWORD") == 0) {
-                strcpy(config.mqtt_hostV2_password, varValue);
-            } else if (strcmp(varName, "MQTT_HOSTV2_PORT") == 0) {
-                config.mqtt_hostV2_port = atoi(varValue);
-            } // Add other variable assignments as needed
-        } else {
-            // Handle case where token doesn't contain a colon
-        }
-        token = strtok_r(NULL, delimiter, &saveptr);
-    }
-
-    free(configCopy); // Free allocated memory
-    return true;
+char hcJsonOutput[240]{0};
+char hcCsvOutput[240]{0};
+const char *parseHealthCheckData(HealthCheck hc, int type = 1) {
+  if (type == 1) {
+    const char *hc_dto = "%s,%d,%d,%i,%i,%i";
+    sprintf(hcCsvOutput, hc_dto,
+            hc.softwareVersion,
+            hc.isWifiConnected ? 1 : 0,
+            hc.isMqttConnected ? 1 : 0,
+            hc.wifiDbmLevel,
+            hc.timestamp,
+            hc.timeRemaining);
+    return hcCsvOutput;
+  } else {
+    const char *json_template = "{\"isWifiConnected\": %d, \"isMqttConnected\": %d, \"wifiDbmLevel\": %i, \"timestamp\": %i}";
+    sprintf(hcJsonOutput, json_template,
+            hc.isWifiConnected ? 1 : 0,
+            hc.isMqttConnected ? 1 : 0,
+            hc.wifiDbmLevel,
+            hc.timestamp);
+    return hcJsonOutput;
+  }
 }
+// --- Metrics data  ---
 
-void setup() {
-    Serial.begin(9600); // Initialize serial communication
-    while (!Serial); // Wait for Serial Monitor to open
-    delay(1000); // Delay for stability
+struct Metrics {
+  float wind_speed = 0;
+  float wind_gust = 0;
+  float rain_acc = 0;
+  float humidity = 0;
+  float temperature = 0;
+  float pressure = 0;
+  int wind_dir = -1;
+  long timestamp;
+} Data;
 
-    const char* configString = "STATION_UID:537\tSTATION_NAME:weather-2\tWIFI_SSID:Gabriel\tWIFI_PASSWORD:2014072276\tMQTT_SERVER:broker.gpicm-ufrj.tec.br\tMQTT_USERNAME:telemetria\tMQTT_PASSWORD:kancvx8thz9FCN5jyq\tMQTT_TOPIC:/prefeituras/macae/estacoes/weather-2\tMQTT_PORT:1883\tINTERVAL:60000\tMQTT_HOSTV2_SERVER:192.168.0.223\tMQTT_HOSTV2_USERNAME:\tMQTT_HOSTV2_PASSWORD:\tMQTT_HOSTV2_PORT:1883";
-    Config config;
-    if (parseConfigString(configString, config)) {
-        // Print parsed values for verification
-        Serial.println("Parsed Config:");
-        Serial.print("station_uid: "); Serial.println(config.station_uid);
-        Serial.print("station_name: "); Serial.println(config.station_name);
-        // Print other values as needed
-    } else {
-        Serial.println("Error: Failed to parse config string.");
-    }
+char metricsjsonOutput[240]{0};
+char metricsCsvOutput[240]{0};
+char csvHeader[200]{0};
+
+void parseData() {
+  // parse measurements data to json
+  const char *json_template = "{\"timestamp\": %i, \"temperatura\": %s, \"umidade_ar\": %s, \"velocidade_vento\": %.2f, \"rajada_vento\": %.2f, \"dir_vento\": %d, \"volume_chuva\": %.2f, \"pressao\": %s, \"uid\": \"%s\", \"identidade\": \"%s\"}";
+  sprintf(metricsjsonOutput, json_template,
+          Data.timestamp,
+          isnan(Data.temperature) ? "null" : String(Data.temperature),
+          isnan(Data.humidity) ? "null" : String(Data.humidity),
+          Data.wind_speed,
+          Data.wind_gust,
+          Data.wind_dir,
+          Data.rain_acc,
+          Data.pressure == -1 ? "null" : String(Data.pressure),
+          config.station_uid,
+          config.station_name);
+
+  // parse measurement data to csv
+  const char *csv_template = "%i,%s,%s,%.2f,%.2f,%d,%.2f,%s,%s,%s\n";
+  sprintf(metricsCsvOutput, csv_template,
+          Data.timestamp,
+          isnan(Data.temperature) ? "null" : String(Data.temperature),
+          isnan(Data.humidity) ? "null" : String(Data.humidity),
+          Data.wind_speed,
+          Data.wind_gust,
+          Data.wind_dir,
+          Data.rain_acc,
+          Data.pressure == -1 ? "null" : String(Data.pressure),
+          config.station_uid,
+          config.station_name);
 }
-
-void lo
