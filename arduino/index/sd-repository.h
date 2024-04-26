bf34d39fe4fe21930ea4c85ece3413473e47b356
Dardrilkael <gabrielfonsecabarreto@gmail.com>
2024-04-25 21:00:00 -0300

    clean code

--- arduino/index/sd-repository.h
+++ arduino/index/sd-repository.h
@@ -48,12 +48,12 @@ void createDirectory(const char * directory){
   Serial.printf("\n     - Diretorio já existe.");
 }
 void parseMQTTString(const char *mqttString, char *username, char *password, char *broker, int &port) {
-    char *ptr = strstr(mqttString, "://");
+    char *ptr = strstr(mqttString, "mqtt://");
     if (ptr == NULL) {
         printf("Invalid MQTT string format!\n");
         return;
     }
-    ptr+=3;
+    ptr+=7;
 
     strlcpy(username, strtok(ptr, ":"), sizeof(config.mqtt_username));
     strlcpy(password, strtok(NULL, "@"), sizeof(config.mqtt_password));
@@ -72,13 +72,11 @@ void loadConfiguration(fs::FS &fs, const char *filename, Config &config, std::st
   while (success == false) {
 
     Serial.printf("\n - Iniciando leitura do arquivo de configuração %s (tentativa: %d)", filename, attemptCount + 1);
-#define V1
+
     if (SD.begin(chipSelectPin, SPI)){
-      #ifdef V1
-      File file = fs.open("/config1.txt");
-      #else
+     
       File file = fs.open(filename);
-      #endif
+
       StaticJsonDocument<512> doc;
 
       if (file){
@@ -90,23 +88,6 @@ void loadConfiguration(fs::FS &fs, const char *filename, Config &config, std::st
           strlcpy(config.wifi_password, doc["WIFI_PASSWORD"] | "", sizeof(config.wifi_password));
           config.interval = doc["INTERVAL"] | 60000;
           strlcpy(config.mqtt_topic, doc["MQTT_TOPIC"] | "unnamed", sizeof(config.mqtt_topic));
-
-#ifdef V1
-        parseMQTTString(doc["MQTT_HOST_V1"],config.mqtt_username,config.mqtt_password,config.mqtt_server,config.mqtt_port);
-        parseMQTTString(doc["MQTT_HOST_V2"]|"mqtt://telemetria:kancvx8thz9FCN5jyq@broker.gpicm-ufrj.tec.br:1883",config.mqtt_hostV2_username,config.mqtt_hostV2_password,config.mqtt_hostV2_server,config.mqtt_hostV2_port);
-#else
-          
-          strlcpy(config.mqtt_server, doc["MQTT_SERVER"] | "", sizeof(config.mqtt_server));
-          strlcpy(config.mqtt_username, doc["MQTT_USERNAME"] | "", sizeof(config.mqtt_username));
-          strlcpy(config.mqtt_password, doc["MQTT_PASSWORD"] | "", sizeof(config.mqtt_password));
-          config.mqtt_port = doc["MQTT_PORT"] | 1883;
-          strlcpy(config.mqtt_topic, doc["MQTT_TOPIC"] | "", sizeof(config.mqtt_topic));
-
-          strlcpy(config.mqtt_hostV2_server, doc["MQTT_HOSTV2_SERVER"] | "", sizeof(config.mqtt_hostV2_server));
-          strlcpy(config.mqtt_hostV2_username, doc["MQTT_HOSTV2_USERNAME"] | "", sizeof(config.mqtt_hostV2_username));
-          strlcpy(config.mqtt_hostV2_password, doc["MQTT_HOSTV2_PASSWORD"] | "", sizeof(config.mqtt_hostV2_password));
-          config.mqtt_hostV2_port = doc["MQTT_HOSTV2_PORT"] | 1884;
-#endif
 
           file.close();
           success = true;
@@ -179,14 +160,6 @@ void storeMeasurement(String directory, String fileName, const char *payload){
   appendFile(SD, path.c_str(), payload);
 }
 
-void readFileToCharArray(const char* filename, char* content, size_t maxLength) {
-  // Open the file
-  File file = SD.open(filename);
-  if (!file) {
-    Serial.println("Failed to open file.");
-    return;
-  }
-}
 
 
 // Adicion uma nova linha de metricas
